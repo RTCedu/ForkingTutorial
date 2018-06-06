4abc2f39a1cb68cce59c70b6c12c95214a6f9fda
Nochtal <nochtal@gmail.com>
2018-06-06 11:35:56 -0700

    Adding Solution
    ForkingAssignment Solution added to repository. This version is the lab work version, and is missing lines of code students will need to add.

---
+++ ForkingAssignment/ForkingAssignment/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace ForkingAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnZero
+            // 
+            this.btnZero.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZero.Location = new System.Drawing.Point(12, 343);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(75, 75);
+            this.btnZero.TabIndex = 0;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.Location = new System.Drawing.Point(12, 262);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(75, 75);
+            this.btnOne.TabIndex = 1;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.Location = new System.Drawing.Point(93, 262);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(75, 75);
+            this.btnTwo.TabIndex = 2;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.Location = new System.Drawing.Point(174, 262);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(75, 75);
+            this.btnThree.TabIndex = 3;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFour.Location = new System.Drawing.Point(12, 181);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(75, 75);
+            this.btnFour.TabIndex = 4;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFive.Location = new System.Drawing.Point(93, 181);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(75, 75);
+            this.btnFive.TabIndex = 5;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNine.Location = new System.Drawing.Point(174, 100);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(75, 75);
+            this.btnNine.TabIndex = 6;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeven.Location = new System.Drawing.Point(12, 100);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(75, 75);
+            this.btnSeven.TabIndex = 7;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEight.Location = new System.Drawing.Point(93, 100);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(75, 75);
+            this.btnEight.TabIndex = 8;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSix.Location = new System.Drawing.Point(174, 181);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(75, 75);
+            this.btnSix.TabIndex = 9;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(255, 181);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 75);
+            this.btnDivide.TabIndex = 10;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(255, 262);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 75);
+            this.btnSubtract.TabIndex = 11;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(255, 100);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 75);
+            this.btnMultiply.TabIndex = 12;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(255, 343);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 75);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEquals.Location = new System.Drawing.Point(93, 343);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(156, 75);
+            this.btnEquals.TabIndex = 14;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Times New Roman", 44.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay.Location = new System.Drawing.Point(12, 19);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(237, 75);
+            this.txtDisplay.TabIndex = 15;
+            this.txtDisplay.Text = "0";
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(255, 19);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 75);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "CE";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 430);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnZero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ForkingAssignment/ForkingAssignment/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ForkingAssignment
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// FORKING ASSIGNMENT
+        /// 
+        /// PLEASE NOTE: SOME CODE STILL NEEDS ADDED FOR FULL FUNCTIONALITY
+        /// 
+        /// FORM OBJECT NAMES
+        /// Buttons:
+        ///     btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive,
+        ///     btnSix, btnSeven, btnEight, btnNine, btn Clear,
+        ///     btnEquals, btnSubtract, btnAdd, btnMultipily, btnDivide
+        /// TextBox
+        ///     txtDisplay
+        /// Global Variables:
+        StringBuilder equation = new StringBuilder(); // used to compile input into one parsable string
+        double answer; // holds the equation's solution upon calculation
+        bool ANSWER_DISPLAYED; // True/False check for answer being displayed
+        char OPERATOR; // +-*/ as input by user for equation
+        /// ANSWER_DISPLAYED will be used to tell what is displayed in 
+        /// txtDisplay, as to know if to erase and start fresh equation
+        /// or to append current equation.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // After initilization, the form loads, during load 
+            // the following lines of code are run. 
+            // This sets up the application for a clean starting point
+            // that the rest of the application can run from.
+            ANSWER_DISPLAYED = true;
+            OPERATOR = 'x';
+            answer = 0;
+            txtDisplay.Text = answer.ToString();
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            AnswerCheck();
+            // concatinate 0 onto string
+            equation.Append("0");
+            // After appending a number to the string update the Display
+            // so the user can see the full string as input.
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 1 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 2 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 3 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 4 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 5 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 6 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate 7 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            AnswerCheck();
+            // concatinate 8 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            AnswerCheck();
+            // concatinate 9 onto string
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate * onto string
+            
+            // Update OPERATOR variable to match chosen operator.
+            OPERATOR = '*';
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate / onto string
+            
+            // Update OPERATOR
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+
+            AnswerCheck();
+            // concatinate - onto string
+            
+            // Update OPERATOR
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AnswerCheck();
+            // concatinate + onto string
+            
+            // Update OPERATOR
+            
+            UpdateEquationDisplay(equation.ToString());
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            // Parse string into equation and calculate result
+            ANSWER_DISPLAYED = true;
+            // The Parsing and calculation is handled by
+            // the ParseCalcEquation() method. It requires a string
+            // input to be passed to it.
+            answer = ParseCalcEquation(equation.ToString());
+            // Display the answer in txtDisplay
+            txtDisplay.Text = answer.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear string of previous input and clear txtDisplay.
+            // set ANSWER_DISPLAYED to true, and restart a the
+            // stringbuilder to a fresh variable.
+            ANSWER_DISPLAYED = true;
+            answer = 0;
+            equation = new StringBuilder();
+            txtDisplay.Text = answer.ToString();
+        }
+
+        public void AnswerCheck()
+        {
+            // This checks if input requires clearing 
+            // the equation that is already held.
+            if (ANSWER_DISPLAYED)
+            {
+                ANSWER_DISPLAYED = false;
+                equation = new StringBuilder();
+            }
+        }
+
+        public void UpdateEquationDisplay(string e)
+        {
+            // This is an example method that takes a string,
+            // returns nothing, and puts that string as the Text
+            // for txtDisplay, thus displaying it to the user.
+            // Though not necessary in this instance, it is often a good
+            // idea to encapsulate repeated actions into a method. They
+            // will often have more than one line of code. For this 
+            // example, it was left as a one line, one action, method.
+            txtDisplay.Text = e;
+        }
+
+        public double ParseCalcEquation(string e)
+        {
+            // instantiate variables to contain the two
+            // integers from the equation. Set to -1, since 
+            // inputing negative numbers is not implemented.
+            int a = -1, b = -1;
+            // Parse the string into two parts, separated by the 
+            // four possible operator characters.
+            string[] parts = e.Split('/', '-', '+', '*');
+            // instantiate a check for each integer, initially set
+            // to false. 
+            bool checkA = false;
+            bool checkB = false;
+            // the rest of the code is encapsulated in an IF logic statement.
+            // If two parts were not parsed out of the string, or if more were,
+            // then skip the code and return -1 (effectively acting as our ERROR 
+            // message to the user.
+            if (parts.Count() == 2)
+            {
+                // First logic will attempt to parse the first piece of data
+                // into an integer. If it cannot, it will exit the method and
+                // return -1.
+                if (!checkA) checkA = int.TryParse(parts[0], out a);
+                else return -1;
+                // If the first variable was successfully parsed into an integer,
+                // the same is done for the second piece of data. If it fails,
+                // the method exits, returning -1.
+                if (!checkB) checkB = int.TryParse(parts[1], out b);
+                else return -1;
+                // This switch statement quickly determines which equation
+                // to use based on the OPERATOR. The equation is executed,
+                // the result being returned by the method.
+                switch (OPERATOR)
+                {
+                    case '+':
+                        return a + b;
+                    case '-':
+                        return a - b;
+                    case '*':
+                        return a * b;
+                    case '/':
+                        return a / b;
+                    default:
+                        return -1;
+                }
+            }
+            else return -1;
+        }
+    }
+}
